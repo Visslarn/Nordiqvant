bfed9a49e893168840cde0d1e65b84b40bdc865d
Visslarn <pontus_bruno@hotmail.com>
2024-10-31 17:38:09 +0100

    Delete nordiqvanta/forms.py

--- nordiqvanta/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class CVUploadForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    cv = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-control'}))
